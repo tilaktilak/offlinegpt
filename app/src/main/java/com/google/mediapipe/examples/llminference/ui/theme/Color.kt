645335dc92ce9225b9a1f203ac2f58ef5b3fcec0
Pierre Tilak <pierre@tilak.io>
2024-11-27 13:23:44 +0100

    Initial commit

---
+++ app/src/main/java/com/google/mediapipe/examples/llminference/ui/theme/Color.kt
@@ -0,0 +1,15 @@
+package com.google.mediapipe.examples.llminference.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val Purple80 = Color(0xFFD0BCFF)
+val PurpleGrey80 = Color(0xFFCCC2DC)
+val Pink80 = Color(0xFFEFB8C8)
+
+val Purple40 = Color(0xFF6650a4)
+val PurpleGrey40 = Color(0xFF625b71)
+val Pink40 = Color(0xFF7D5260)
+
+val PrimaryDark = Color(0xEDEFF0)
+val SecondaryDark = Color(0x156C91)
+val TertiaryDark = Color(0x22E0B2)
